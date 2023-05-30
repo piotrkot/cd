f9146ba3a0dfcd12b9d8fdf6be0205c7cf3af704
Piotr Kotlicki <piotr.kotlicki@gmail.com>
2023-05-30 23:31:19 +0200

    Adding rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const slowDown = require('express-slow-down');
 const { logger } = require('./log.js');
 const auth = require('./auth.js');
 const dishes = require('./routes/dishes.js');
@@ -8,6 +9,13 @@ const ratings = require('./routes/ratings.js');
 const app = express();
 const port = process.env.APPPORT;
 
+const speedLimiter = slowDown({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  delayAfter: 5, // allow 5 requests per 5 minutes, then add 500ms delays for each next request
+  delayMs: 500,
+});
+
+app.use(speedLimiter);
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/authenticate', async (req, res) => {
